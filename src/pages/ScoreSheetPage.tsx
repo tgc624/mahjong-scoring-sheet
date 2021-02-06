1c6fadb7fc0f2f3d97cabd9cb5d441ee8041d87d
tgc624 <fummy.tgc@gmail.com>
2021-02-07 05:18:42 +0900

    Implement game-score logic in useScoreSheetTable

--- src/pages/ScoreSheetPage.tsx
+++ src/pages/ScoreSheetPage.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Game, Player, Players, useScoreSheetTable } from "../utils/util";
 
-const ScoreSheet = ({ players }: { players: Players }) => {
+const ScoreSheet = ({
+  players,
+  games,
+}: {
+  players: Players;
+  games: Game[];
+}) => {
   const [playerNames, totalScores, scoresRows] = useScoreSheetTable(
     players,
-    []
+    games
   );
   return (
     <table>
@@ -39,6 +45,22 @@ const ScoreSheet = ({ players }: { players: Players }) => {
             </td>
           ))}
         </tr>
+        {scoresRows.map((scores, rowIndex) => (
+          <tr key={rowIndex}>
+            <td>{rowIndex + 1}</td>
+            {scores.map((score, colIndex) => (
+              <td
+                key={colIndex}
+                style={{
+                  textAlign: "center",
+                  width: `calc(100% / ${playerNames.length})`,
+                }}
+              >
+                {score}
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </table>
   );
@@ -46,7 +68,18 @@ const ScoreSheet = ({ players }: { players: Players }) => {
 
 export const ScoreSheetPage = () => {
   const { sheetId } = useParams<{ sheetId: string }>();
-  const games: Game[] = [];
+  const games: Game[] = [
+    {
+      id: "1",
+      scores: [
+        { uid: "a", score: 1000 },
+        { uid: "b", score: 2000 },
+        { uid: "c", score: 30000 },
+        { uid: "d", score: -40000 },
+      ],
+      createdAt: 1,
+    },
+  ];
   const players: [Player, Player, Player, Player] = [
     { uid: "a", name: "佐藤" },
     { uid: "b", name: "鈴木" },
@@ -55,7 +88,7 @@ export const ScoreSheetPage = () => {
   ];
   return (
     <div>
-      <ScoreSheet players={players} />
+      <ScoreSheet players={players} games={games} />
     </div>
   );
 };
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -3,8 +3,11 @@ export type Players = [Player, Player, Player, Player];
 export type Game = {
   id: string;
   scores: { uid: string; score: number }[];
-  cratedAt: number;
+  createdAt: number;
 };
+
+const sortBy = <T>(sortTarget: keyof T) => (array: T[]): T[] =>
+  array.sort((a, b) => (a < b ? -1 : 1));
 
 export const useScoreSheetTable = (
   players: Players,
@@ -23,7 +26,19 @@ export const useScoreSheetTable = (
 
   const totalScores = [0, 0, 0, 0] as [number, number, number, number];
 
-  const scoresRows = [] as [number, number, number, number][];
+  const sortedGames = sortBy("createdAt")(games) as Game[];
+  const scoresRows = sortedGames.map((game) => {
+    const scoreByUid = Object.fromEntries(
+      game.scores.map(({ uid, score }) => [uid, score])
+    );
+    const sortedScores = sortedUids.map((uid) => scoreByUid[uid]) as [
+      number,
+      number,
+      number,
+      number
+    ];
+    return sortedScores;
+  });
 
   return [sortedPlayerNames, totalScores, scoresRows];
 };
