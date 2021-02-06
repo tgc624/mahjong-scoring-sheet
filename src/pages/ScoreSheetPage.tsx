47fa47b5cf67ca0d388f00cc39303bd2247bd73c
tgc624 <fummy.tgc@gmail.com>
2021-02-07 02:40:43 +0900

    Init score table

--- src/pages/ScoreSheetPage.tsx
+++ src/pages/ScoreSheetPage.tsx
@@ -1,7 +1,60 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { callbackify } from "util";
+
+type Player = { uid: string; name: string };
+type Game = {
+  id: string;
+  scores: { uid: string; score: number }[];
+  cratedAt: number;
+};
+const ScoreSheet = ({
+  players,
+}: {
+  players: [Player, Player, Player, Player];
+}) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          {players.map((player) => (
+            <th
+              key={player.uid}
+              style={{
+                textAlign: "center",
+                width: `calc(100% / ${players.length})`,
+              }}
+            >
+              {player.name}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Σ</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 export const ScoreSheetPage = () => {
   const { sheetId } = useParams<{ sheetId: string }>();
-  return <div>{sheetId},score sheet page!</div>;
+  const players: [Player, Player, Player, Player] = [
+    { uid: "a", name: "佐藤" },
+    { uid: "b", name: "鈴木" },
+    { uid: "c", name: "高橋" },
+    { uid: "d", name: "田中" },
+  ];
+  return (
+    <div>
+      <ScoreSheet players={players} />
+    </div>
+  );
 };
